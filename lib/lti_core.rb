8a0567abeae233f63ec3824c7032be262398820f
Aaron F Stanton <afstanton@gmail.com>
2024-03-23 11:06:57 -0400

    Initial commit

---
+++ lib/lti_core.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lti_core/version"
+
+module LtiCore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lti_core/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LtiCore
+  VERSION = "0.0.1"
+end

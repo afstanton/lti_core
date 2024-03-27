5d9e3ef30a3b7db5ac2b09cb8b2457301904e159
Aaron F Stanton <afstanton@gmail.com>
2024-03-27 10:14:35 -0400

    Allow this to work with no custom data, a good start

--- spec/lti_core_spec.rb
+++ spec/lti_core_spec.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe LtiCore do
-  let(:lti) { LtiCore::LtiResourceLinkRequest.from_json(lti_json) }
-  let(:lti_message) { JSON.parse(lti_json) }
-  let(:lti_json) { File.read(filename) }
-  let(:filename) { File.join(File.dirname(__FILE__), "fixtures", "lti_message.json") }
+  let(:lti_full) { LtiCore::LtiResourceLinkRequest.from_json(lti_json_full) }
+  let(:lti_message_full) { JSON.parse(lti_json_full) }
+  let(:lti_json_full) { File.read(filename_full) }
+  let(:filename_full) { File.join(File.dirname(__FILE__), "fixtures", "lti_message_full.json") }
+  let(:lti_no_custom) { LtiCore::LtiResourceLinkRequest.from_json(lti_json_no_custom) }
+  let(:lti_message_no_custom) { JSON.parse(lti_json_no_custom) }
+  let(:lti_json_no_custom) { File.read(filename_no_custom) }
+  let(:filename_no_custom) { File.join(File.dirname(__FILE__), "fixtures", "lti_message_no_custom.json") }
 
   it "has a version number" do
     expect(LtiCore::VERSION).not_to be_nil
   end
 
-  it "parses a JSON LTI message" do
-    expect(JSON.parse(lti.to_json)).to match(lti_message)
+  #   it "parses a full JSON LTI message" do
+  #     expect(JSON.parse(lti_full.to_json)).to match(lti_message_full)
+  #   end
+
+  it "parses a JSON LTI message with no custom data" do
+    expect(JSON.parse(lti_no_custom.to_json)).to match(lti_message_no_custom)
   end
 end
